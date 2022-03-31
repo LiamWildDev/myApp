f243549f7e2ec1678ecea791a3ef16006f58318e
LiamWildDev <liam_wildeman@yahoo.com>
2022-03-31 14:34:25 -0400

    test branche 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,7 +3,7 @@
 namespace ConsoleApp2
 {
     class Program
-    { // ceci est un test
+    { // test branche 2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
