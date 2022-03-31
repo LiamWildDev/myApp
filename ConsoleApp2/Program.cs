093c3e60a0dd9e2df102af708f65188545781309
LiamWildDev <liam_wildeman@yahoo.com>
2022-03-31 12:39:24 -0400

    voici un test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,11 +3,11 @@
 namespace ConsoleApp2
 {
     class Program
-    {
+    { // ceci est un test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
-    }
+    } 
 }
